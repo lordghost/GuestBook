ec06dadf61e88b965583b93ab450fe0538186f7d
Oleg <OlegKulik91@yandex.ru>
2012-11-16 20:14:27 +0200

    views added

---
+++ src/Acme/GuestBookBundle/Form/GuestType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Acme\GuestBookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GuestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('message')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\GuestBookBundle\Entity\Guest'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'acme_guestbookbundle_guesttype';
+    }
+}
